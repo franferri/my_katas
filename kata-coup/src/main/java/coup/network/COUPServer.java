53fbc839afc627649da1d119973b11bef4506d43
Fran <franferri@gmail.com>
2019-05-25 15:33:45 +0100

    Added server to play by command line

---
+++ kata-coup/src/main/java/coup/network/COUPServer.java
@@ -0,0 +1,45 @@
+package coup.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class COUPServer {
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.err.println("Usage: java KnockKnockServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        try (
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+
+            String inputLine, outputLine;
+
+            // Initiate conversation with client
+            COUPServerProtocol kkp = new COUPServerProtocol();
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye.")) break;
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
