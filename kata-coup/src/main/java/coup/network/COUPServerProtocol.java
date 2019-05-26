12316d01e8a49f3b25b2a64414d0760c240f9ad0
Fran <franferri@gmail.com>
2019-05-26 03:25:26 +0100

    notes added to the protocol class

--- kata-coup/src/main/java/coup/network/COUPServerProtocol.java
+++ kata-coup/src/main/java/coup/network/COUPServerProtocol.java
@@ -8,6 +8,13 @@ public class COUPServerProtocol {
     private static final int JUST_CONNECTED = 0;
     private static final int JOINED_THE_TABLE = 1;
     private static final int WAITING = 2;
+
+    private static final int WAITING_FOR_PLAYER_TO_PLAY = 3;
+
+    private static final int DOING_ACTION = 4;
+    private static final int CALLING_THE_BLUFF = 5;
+    private static final int BLOCKING_ACTION = 6;
+
     private static final int ACTIVE = 10000;
 
     private int state = JUST_CONNECTED;
@@ -45,12 +52,55 @@ public class COUPServerProtocol {
             theOutput.add("\033[H\033[2J");
             theOutput.addAll(ASCIIArt.showCurrentTable());
 
+            state = WAITING_FOR_PLAYER_TO_PLAY;
+
+        } else if (state == WAITING_FOR_PLAYER_TO_PLAY && theInput.equals("income")) {
+
+            state = DOING_ACTION;
+
+        } else if (state == WAITING_FOR_PLAYER_TO_PLAY && theInput.equals("foreign_aid")) {
+
+            state = DOING_ACTION;
+
+        } else if (state == WAITING_FOR_PLAYER_TO_PLAY && theInput.equals("coup7")) {
+
+            state = DOING_ACTION;
+
+        } else if (state == WAITING_FOR_PLAYER_TO_PLAY && theInput.equals("coup10")) {
+
+            state = DOING_ACTION;
+
+        } else if (state == WAITING_FOR_PLAYER_TO_PLAY && theInput.equals("tax")) {
+
+            state = DOING_ACTION;
+
+        } else if (state == DOING_ACTION && theInput.equals("call the bluff")) {
+
+            state = CALLING_THE_BLUFF;
+
+        } else if (state == DOING_ACTION && theInput.equals("block")) {
+
+            state = BLOCKING_ACTION;
+
+        } else if (state == BLOCKING_ACTION && theInput.equals("call the bluff")) {
+
+            state = CALLING_THE_BLUFF;
 
         } else if (theInput.equals("Game over")) {
             theOutput.add("Game over");
         } else {
             theOutput.add("Unknown parameter: " + theInput);
         }
+
+        // IF A PLAYER GETS DISCONNECTED, THE GAME WILL REVEAL HIS CARDS INMEDIATELY
+        // IF A PLAYER IS TAKING MORE THAN N MINUTES TO PLAY, WILL DIE
+
+        // EACH PLAYER SEES THE COUNT DOWN TO BE ABLE TO BLOCK OR CALL THE BLUF, DURING THIS TIME
+        // THE ACTION IS NOT EXECUTED, SO OTHER PLAYERS CAN ACT
+
+        // TIMES APPEAR IN THE BOARD, TO EACH USER HIS OWN
+
+        // añadir un test al juego: un jugador q no está en su turno, intenta hacer una acción
 
         return theOutput;
     }
